5f8d0d3daab126465b4877aa3b2f9417bb53e1c2
Oleksandr Kostenko <sanyokkua@gmail.com>
2025-07-30 10:28:47 +0200

    Cleanup

--- src/llmedit/core/services/languages/language_provider.py
+++
@@ -1,6 +0,0 @@
-from core.abstracts.providers import LanguageProvider
-
-
-class AppLanguageProvider(LanguageProvider):
-    def get_supported_languages(self) -> list[str]:
-        return ["English", "Ukrainian", "Croatian"]
--- src/llmedit/main.py
+++ src/llmedit/main.py
@@ -3,20 +3,16 @@ from pathlib import Path
 
 from PyQt6.QtWidgets import QApplication
 
-from core.abstracts.app_context import AppContext
-from core.app_di_context import get_app_context
-from ui.main_window import MainWindow
 
 APP_ROOT_PATH = Path(__file__).resolve().parents[2]
 
 
-def start_application(context: AppContext):
+def start_application():
     app = QApplication(sys.argv)
-    window = MainWindow(context)
-    window.show()
+    # window = MainWindow()
+    # window.show()
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    app_context = get_app_context(APP_ROOT_PATH)
-    start_application(app_context)
+    start_application()
